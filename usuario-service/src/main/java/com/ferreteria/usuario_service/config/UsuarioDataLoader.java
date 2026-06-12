d291967521802b307b6c22d25e9973fea0ebc8e1
Paulo <paulo.icz08@gmail.com>
2026-06-11 20:59:35 -0400

    feat(usuario): implementa DataLoader para inyeccion inicial de productos e inhabilita liquibase local

---
+++ usuario-service/src/main/java/com/ferreteria/usuario_service/config/UsuarioDataLoader.java
@@ -0,0 +1,43 @@
+package com.ferreteria.usuario_service.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+// IMPORTANTE: Cambia estos imports por la ruta real de tus clases
+import com.ferreteria.usuario_service.model.Usuario;
+import com.ferreteria.usuario_service.repository.UsuarioRepository;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j 
+public class UsuarioDataLoader implements CommandLineRunner {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        // Verifica si la base de datos está vacía
+        if (usuarioRepository.count() == 0) {
+            log.info("Iniciando la carga de usuarios por defecto...");
+
+            Usuario admin = new Usuario();
+            admin.setNombre("Administrador");
+            admin.setEmail("admin@ferreteria.cl");
+
+
+            Usuario cliente = new Usuario();
+            cliente.setNombre("Cliente Frecuente");
+            cliente.setEmail("cliente@ferreteria.cl");
+
+            usuarioRepository.save(admin);
+            usuarioRepository.save(cliente);
+
+            log.info("¡Usuarios cargados exitosamente!");
+        } else {
+            log.info("La base de datos ya contiene usuarios. Omitiendo carga.");
+        }
+    }
+}
