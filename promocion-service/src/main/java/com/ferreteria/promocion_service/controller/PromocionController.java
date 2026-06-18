6258d982f95d747d43421c80d8b7707a03014494
Paulo <paulo.icz08@gmail.com>
2026-06-18 14:35:07 -0400

    feat(promocion-service): implementar enlaces HATEOAS en validaciones y listas

--- promocion-service/src/main/java/com/ferreteria/promocion_service/controller/PromocionController.java
+++ promocion-service/src/main/java/com/ferreteria/promocion_service/controller/PromocionController.java
@@ -5,6 +5,13 @@ import com.ferreteria.promocion_service.model.Promocion;
 import com.ferreteria.promocion_service.service.PromocionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/promociones")
@@ -35,8 +43,17 @@ public class PromocionController {
     // GET: Ver todas las promociones
     // http://localhost:9090/api/promociones
     @GetMapping
-    public ResponseEntity<List<Promocion>> obtenerTodas() {
-        return ResponseEntity.ok(promocionService.obtenerTodas());
+    public ResponseEntity<CollectionModel<EntityModel<Promocion>>> obtenerTodas() {
+        List<Promocion> promociones = promocionService.obtenerTodas();
+        
+        // Envolvemos cada promoción y le agregamos un link para validar su propio código
+        List<EntityModel<Promocion>> promocionesModel = promociones.stream()
+            .map(promocion -> EntityModel.of(promocion,
+                linkTo(methodOn(this.getClass()).validarCodigo(promocion.getCodigo())).withRel("validar-codigo")))
+            .collect(Collectors.toList());
+            
+        WebMvcLinkBuilder linkSelf = linkTo(methodOn(this.getClass()).obtenerTodas());
+        return ResponseEntity.ok(CollectionModel.of(promocionesModel, linkSelf.withSelfRel()));
     }
 
     // GET: Validar código y obtener descuento
@@ -50,7 +67,15 @@ public class PromocionController {
             Map<String, Double> response = new HashMap<>();
             response.put("descuento", porcentaje);
             
-            return ResponseEntity.ok(response);
+            // Envolvemos el Map en HATEOAS
+            EntityModel<Map<String, Double>> recurso = EntityModel.of(response);
+            WebMvcLinkBuilder linkSelf = linkTo(methodOn(this.getClass()).validarCodigo(codigo));
+            WebMvcLinkBuilder linkTodas = linkTo(methodOn(this.getClass()).obtenerTodas());
+            
+            recurso.add(linkSelf.withSelfRel());
+            recurso.add(linkTodas.withRel("todas-las-promociones"));
+            
+            return ResponseEntity.ok(recurso);
         } catch (RuntimeException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
